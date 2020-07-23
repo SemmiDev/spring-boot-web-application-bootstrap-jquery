4b7012e54c32f3218faf89e4be242b19509bd0b5
SemmiDev <sammidev4@gmail.com>
2020-07-23 16:25:34 +0700

    init

---
+++ src/main/java/com/sam/demo/configuration/Test.java
@@ -0,0 +1,13 @@
+package com.sam.demo.configuration;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test {
+
+    @org.junit.Test
+    public void contextLoads(){}
+}
---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,1 @@
+alert("Hello! i'm active");
